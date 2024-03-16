a314014314a96cb669d3fd04a69b1ae812fcb82a
grapeVine <treeui.old@gmail.com>
2024-03-16 22:36:18 +0800

    diff

---
+++ voyager/Core/Hotmap/ViewModel/HotmapViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  HotmapViewModel.swift
+//  voyager
+//
+//  Created by grapestree on 2023/12/27.
+//
+
+import Foundation
---
+++ voyager/Model/Strategy.swift
@@ -0,0 +1,8 @@
+//
+//  Strategy.swift
+//  voyager
+//
+//  Created by grapestree on 2023/12/27.
+//
+
+import Foundation
