3f5b7dd2718bca426b4228b5deae55fa1bd3dde0
grapeVine <treeui.old@gmail.com>
2024-03-26 22:42:01 +0800

    update impl

---
+++ voyager/Model/License.swift
@@ -0,0 +1,8 @@
+//
+//  License.swift
+//  voyager
+//
+//  Created by grapestree on 2024/3/25.
+//
+
+import Foundation
---
+++ voyager/Model/StoryItem.swift
@@ -0,0 +1,8 @@
+//
+//  StoryItem.swift
+//  voyager
+//
+//  Created by grapestree on 2024/3/26.
+//
+
+import Foundation
