3141522ce8950cfe1ae89711a4b19dee86aa9eff
grapeVine <treeui.old@gmail.com>
2024-03-31 21:54:12 +0800

    update group impl

--- voyager/Core/Branch/ViewModels/GroupViewModel.swift
+++ voyager/Core/Branch/ViewModels/GroupViewModel.swift
@@ -10,7 +10,8 @@ import Foundation
 
 class GroupViewModel: ObservableObject {
     @Published var name: String
-    @Published var user:User
+    @Published var user: User
+    @Published var info: BranchGroup
     @Published var projectList: [Project]
     @Published var members: [User]
     @Published var groupProfile: GroupProfile
@@ -20,22 +21,23 @@ class GroupViewModel: ObservableObject {
         self.projectList = [Project]()
         self.members = [User]()
         self.groupProfile = GroupProfile()
+        self.info  = BranchGroup()
     }
     
     func fetchGroupInfo() async{
-        
+        self.info  = BranchGroup()
     }
     
     func fetchGroupProfile() async {
-        
+        self.groupProfile = GroupProfile()
     }
     
     func fetchGroupMembers() async {
-        
+        self.members = [User]()
     }
     
     func fetchGroupProjects() async {
-        
+        self.projectList = [Project]()
     }
     
     func createNewProjectInGroup() async{
