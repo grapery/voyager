c2de5e00f077497b2ba438c4d3b96c374f137924
葡萄树 <treeui.old@gmail.com>
2025-06-02 11:44:00 +0800

    update impl

--- voyager/Core/Branch/VIews/GroupView.swift
+++ voyager/Core/Branch/VIews/GroupView.swift
@@ -29,7 +29,7 @@ struct GroupView: View {
                 
                 GroupViewListView(viewModel: viewModel)
             }
-            .sheet(isPresented: $isShowingNewGroupView) {
+            .fullScreenCover(isPresented: $isShowingNewGroupView) {
                 NewGroupView(userId: viewModel.user.userID, viewModel: viewModel)
             }
             .onAppear {
